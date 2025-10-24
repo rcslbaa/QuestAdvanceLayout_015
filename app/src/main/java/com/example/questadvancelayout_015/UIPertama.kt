f2e080f471255075018bfb8d6ff2f7c9c2bf6f5c
rcslbaa <ifasmarani3125@gmail.com>
2025-10-24 16:04:34 +0700

    menambahkan text "Teknologi Informasi" di bagian paling atas

--- app/src/main/java/com/example/questadvancelayout_015/UIPertama.kt
+++ app/src/main/java/com/example/questadvancelayout_015/UIPertama.kt
@@ -33,3 +33,9 @@ fun ActivitasPertama(modifier: Modifier){
         .fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally
     ){
+        Text(
+            text = stringResource(id= R.string.prodi),
+            fontSize = 30.sp,
+            fontWeight = FontWeight.Bold
+        )
+
