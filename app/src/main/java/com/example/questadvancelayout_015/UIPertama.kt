46a77a567d26b01457bf00ba8509ccdd5b500610
rcslbaa <ifasmarani3125@gmail.com>
2025-10-24 15:20:55 +0700

    menambahkan tampilan luar

---
+++ app/src/main/java/com/example/questadvancelayout_015/UIPertama.kt
@@ -0,0 +1,19 @@
+package com.example.questadvancelayout_015
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun ActivitasPertama(modifier: Modifier){
