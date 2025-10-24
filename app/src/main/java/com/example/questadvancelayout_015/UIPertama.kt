2724c7881a87c0e65974bdd1af899d0b968dbff2
rcslbaa <ifasmarani3125@gmail.com>
2025-10-24 16:14:47 +0700

    menambahkan gambar logo umy

--- app/src/main/java/com/example/questadvancelayout_015/UIPertama.kt
+++ app/src/main/java/com/example/questadvancelayout_015/UIPertama.kt
@@ -59,3 +59,12 @@ fun ActivitasPertama(modifier: Modifier){
                 verticalAlignment = Alignment.CenterVertically,
                 modifier = Modifier.padding(all = 10.dp)
             ){
+                val gambar = painterResource(id = R.drawable.logo_umy)
+                Image(
+                    painter = gambar,
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(size = 100.dp)
+                        .padding(all = 5.dp)
+                )
+
